01de932ffd1978a16b5dca12e1c409b4d8355ada
Ansis Maleckis <maleckisansis@gmail.com>
2022-08-29 21:34:38 +0300

    finished stage 2

--- tetris/game.py
+++ tetris/game.py
@@ -1,4 +1,3 @@
-
 O = [[4, 14, 15, 5]]
 I = [[4, 14, 24, 34], [3, 4, 5, 6]]
 S = [[5, 4, 14, 13], [4, 14, 15, 25]]
@@ -25,36 +24,49 @@ class Array:
     def down(self):
         for piece in self.data:
             for i in range(len(piece)):
-                piece[i] = (piece[i] + self.cols) % self.size
+                piece[i] = piece[i] + self.cols
+                if piece[i] > self.size:
+                    piece[i] %= self.size
 
     def rotate(self):
-        self.index = (self.index + 1) % len(self.data)
         self.down()
+        self.index = (self.index + 1) % len(self.data)
 
+    def left(self):
+        self.move(left=True)
 
+    def right(self):
+        self.move(left=False)
 
+    def move(self, left):
+        self.down()
+        for piece in self.data:
+            for i in range(len(piece)):
+                if not left:
+                    if piece[i] % self.cols == 0:
+                        piece[i] = piece[i] + 1 - self.cols
+                    else:
+                        piece[i] = piece[i] + 1
+                elif left:
+                    if piece[i] % self.cols == 1:
+                        piece[i] = piece[i] + self.cols - 1
+                    else:
+                        piece[i] = piece[i] - 1
 
-def main():
-    switcher = {
-        "T": T,
-        "J": J,
-        "L": L,
-        "O": O,
-        "I": I,
-        "S": S,
-        "Z": Z
-    }
-
-    piece = switcher.get(input(), [[]])
+def get_dimenssions():
     while True:
         try:
             dimenssions = input().split(" ")
             cols = int(dimenssions[0])
             rows = int(dimenssions[1])
-            break
+            return cols, rows
         except Exception as e:
             print(e)
 
+def main():
+    switcher = {"T": T, "J": J, "L": L, "O": O, "I": I, "S": S, "Z": Z}
+    piece = switcher.get(input(), [[]])
+    cols, rows = get_dimenssions()
     array = Array(piece, cols, rows)
     array.display(False)
     array.display(True)
@@ -65,14 +77,12 @@ def main():
             break
         elif instruction == "rotate":
             array.rotate()
-        # elif instruction == "left":
-        #     left(array)
-        # elif instruction == "righ":
-        #     right(array)
-        elif instruction == "down":
+        elif instruction == "left":
+            array.left()
+        elif instruction == "right":
+            array.right()
+        else:
             array.down()
-        # else:
-        #     pass
         array.display(True)
 
 if __name__ == "__main__":
