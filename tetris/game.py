205976d4e6692dd9d601e54eb939ffba2a4bddcd
Ansis Maleckis <maleckisansis@gmail.com>
2022-08-29 20:29:56 +0300

    some progress with stage 2

--- tetris/game.py
+++ tetris/game.py
@@ -1,23 +1,38 @@
 
-X = [[]]
-O = [[5, 6, 9, 10]]
-I = [[1, 5, 9, 13], [4, 5, 6, 7]]
-S = [[6, 5, 9, 8], [5, 9, 10, 14]]
-Z = [[4, 5, 9, 10], [2, 5, 6, 9]]
-L = [[1, 5, 9, 10], [2, 4, 5, 6], [1, 2, 6, 10], [4, 5, 6, 8]]
-J = [[2, 6, 9, 10], [4, 5, 6, 10], [1, 2, 5, 9], [0, 4, 5, 6]]
-T = [[1, 4, 5, 6], [1, 4, 5, 9], [4, 5, 6, 9], [1, 5, 6, 9]]
+O = [[4, 14, 15, 5]]
+I = [[4, 14, 24, 34], [3, 4, 5, 6]]
+S = [[5, 4, 14, 13], [4, 14, 15, 25]]
+Z = [[4, 5, 15, 16], [5, 15, 14, 24]]
+L = [[4, 14, 24, 25], [5, 15, 14, 13], [4, 5, 15, 25], [6, 5, 4, 14]]
+J = [[5, 15, 25, 24], [15, 5, 4, 3], [5, 4, 14, 24], [4, 14, 15, 16]]
+T = [[4, 14, 24, 15], [4, 13, 14, 15], [5, 15, 25, 14], [4, 5, 6, 15]]
 
-def display(arrays):
+class Array:
 
-    if len(arrays) >= 2:
-        arrays.append(arrays[1])
+    def __init__(self, data, cols, rows):
+        self.index = 0
+        self.data = data
+        self.cols = cols
+        self.rows = rows
+        self.size = cols * rows
 
-    for array in arrays:
+    def display(self, show):
         print()
-        for i in range(0, 16):
-            print("0" if i in array else "-", "", sep=" ", end="")
-            print("\n" if (i + 1) % 4 == 0 else "", end="")
+        for i in range(0, self.size):
+            print("0" if show and i in self.data[self.index] else "-", "", sep=" ", end="")
+            print("\n" if (i + 1) % self.cols == 0 else "", end="")
+
+    def down(self):
+        for piece in self.data:
+            for i in range(len(piece)):
+                piece[i] = (piece[i] + self.cols) % self.size
+
+    def rotate(self):
+        self.index = (self.index + 1) % len(self.data)
+        self.down()
+
+
+
 
 def main():
     switcher = {
@@ -29,9 +44,36 @@ def main():
         "S": S,
         "Z": Z
     }
-    array = switcher.get(input(), [])
-    display(X + array * (4 // len(array)))
 
+    piece = switcher.get(input(), [[]])
+    while True:
+        try:
+            dimenssions = input().split(" ")
+            cols = int(dimenssions[0])
+            rows = int(dimenssions[1])
+            break
+        except Exception as e:
+            print(e)
+
+    array = Array(piece, cols, rows)
+    array.display(False)
+    array.display(True)
+
+    while True:
+        instruction = input()
+        if instruction == "exit":
+            break
+        elif instruction == "rotate":
+            array.rotate()
+        # elif instruction == "left":
+        #     left(array)
+        # elif instruction == "righ":
+        #     right(array)
+        elif instruction == "down":
+            array.down()
+        # else:
+        #     pass
+        array.display(True)
 
 if __name__ == "__main__":
     main()
